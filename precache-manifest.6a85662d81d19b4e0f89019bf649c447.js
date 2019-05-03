94e54ec4fe38b04e0866389a3e5c93351b9f1c32
Hur Ali <hurali97@gmail.com>
2019-05-03 18:28:36 +0500

    Updates

---
+++ precache-manifest.6a85662d81d19b4e0f89019bf649c447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed39ae4b691e2a3eeb840b4f279d98a",
+    "url": "/Billing-Calculator/index.html"
+  },
+  {
+    "revision": "4035aacae83cd5bf999e",
+    "url": "/Billing-Calculator/static/css/main.db334fd1.chunk.css"
+  },
+  {
+    "revision": "26fbac70f00a5c2477e6",
+    "url": "/Billing-Calculator/static/js/2.de3d5109.chunk.js"
+  },
+  {
+    "revision": "4035aacae83cd5bf999e",
+    "url": "/Billing-Calculator/static/js/main.6ea7a103.chunk.js"
+  },
+  {
+    "revision": "e8739c796edf7643c9bb",
+    "url": "/Billing-Calculator/static/js/runtime~main.e6d8bf77.js"
+  }
+]);
