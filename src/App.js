b7eb024a5788567e0ed2dd4f594a598266c568e0
Hur Ali <hurali97@gmail.com>
2019-07-04 23:02:16 +0500

    particles added

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import SimpleCard from './Component/Card';
 import Excel from './Component/Excel';
 import Button from '@material-ui/core/Button';
 import { Helmet } from "react-helmet";
-
+import Particles from 'react-particles-js';
 
 
 class App extends React.Component {
 
 
   render() {
-   
+
 
     return (
 
@@ -22,11 +22,16 @@ class App extends React.Component {
 
         {/* <SimpleCard onItem={this.props.onItem} onAmount={this.props.onAmount} onAdd={this.props.onAdd} /> */}
         <Helmet>
-        <title>Billing Calculator</title>
-        <meta name="description" content="A Billing Calculator to help you with your calculations of groceries and other stuff" />
-        <meta name="keywords" content="billing-calculator,billing,grocery,calculator" />
-      </Helmet>
-
+          <title>Billing Calculator</title>
+          <meta name="description" content="A Billing Calculator to help you with your calculations of groceries and other stuff" />
+          <meta name="keywords" content="billing-calculator,billing,grocery,calculator" />
+        </Helmet>
+        <Particles style={{
+          position: 'fixed ', zIndex: '-1', top: '0',
+          right: '0',
+          bottom: '0',
+          left: '0'
+        }} />
         <SimpleCard />
 
 
@@ -40,22 +45,22 @@ class App extends React.Component {
               {(this.props.show === 'yes')
                 ? (
                   <div>
-                  <CustomizedTable summary={this.props.summary} />
-                  <Button size="small" style={{ marginTop: '2vh', backgroundColor: '#00b5ad', color: 'white' }} onClick={this.props.onNewBill} variant="contained"  >
-                  New Bill
+                    <CustomizedTable summary={this.props.summary} />
+                    <Button size="small" style={{ marginTop: '2vh', backgroundColor: '#00b5ad', color: 'white' }} onClick={this.props.onNewBill} variant="contained"  >
+                      New Bill
                  </Button>
-                 < Excel/>
-                <br></br>
-                 </div>
+                    < Excel />
+                    <br></br>
+                  </div>
                 )
                 : (this.props.show === 'none') ? <h3>Please Add Items, Quantity And Amount !</h3>
-                : (this.props.show === 'string') ? <h3>Please Input A Number In Amount/Quantity !</h3>
+                  : (this.props.show === 'string') ? <h3>Please Input A Number In Amount/Quantity !</h3>
                     : console.log("no")
               }
             </Col>
 
             <Col md={3} sm={1} xs={1}>
-          
+
             </Col>
           </Row>
         </Container>
@@ -72,8 +77,8 @@ const mapStateToProps = state => {
   return {
     show: state.show,
     summary: state.summary,
-    
-      };
+
+  };
 };
 
 const mapDispachToProps = dispatch => {
